d8831037c1b19bc989f7763ef459dda9e8ad87db
izamisa <124897143+izamisa@users.noreply.github.com>
2023-03-16 23:28:53 +0100

    task2

---
+++ task1.java
@@ -0,0 +1,15 @@
+public class Cafe {
+    void display() {
+        System.out.println("1. Expresso");
+        System.out.println("2. Americano");
+        System.out.println("3. Macchiato");
+        System.out.println("4. Flat White");
+        System.out.println("5. Latte");
+
+
+        public static void main(String args[]) {
+            private ArrayList<String> coffeeMenu;
+
+        }
+
+    }
